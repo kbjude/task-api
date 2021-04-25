1907746cbe5f5d548336bd8633dbd5435ba7d47d
kbjude <kbjude@gmail.com>
2021-04-25 23:40:51 +0300

    Style lint

--- app/controllers/api/v1/task_controller.rb
+++ app/controllers/api/v1/task_controller.rb
@@ -1,10 +1,10 @@
 module API
-    module V1
-        class TasksController < ApplicationController
-            def index
-                @tasks = Tasks.order('created_at DESC');
-                render json: {status: 'SUCCESS', message:'Loaded articles', data:tasks}, status: :ok
-            end
-        end
+  module V1
+    class TasksController < ApplicationController
+      def index
+        @tasks = Tasks.order('created_at DESC');
+        render json: { status: 'SUCCESS', message: 'Loaded articles', data: tasks }, status: :ok
+      end
     end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    # validates :name, prescence: true
-    # validates :description, prescence: true
+  # validates :name, prescence: true
+  # validates :description, prescence: true
 end
